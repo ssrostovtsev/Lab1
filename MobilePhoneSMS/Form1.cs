020aae229f9c053ff030756a67898bf959308b3f
SRV <SRV@DK01WV6035>
2017-12-25 23:05:12 +0100

    Lab3 with delegates and events has been added.

--- MobilePhoneSMS/Form1.cs
+++ MobilePhoneSMS/Form1.cs
@@ -1,21 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MobilePhoneClassLib;
+using static MobilePhoneClassLib.SMSProvider;
 
 namespace MobilePhoneSMS {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+            SMSProv = new SMSProvider();
+            SMSProv.SMSRecieved += new SMSRecievedHandler(ShowSMS);
+            string[] formats = new string[] { "None", "Start with DateTime", "End with DateTime", "Lowercase", "Uppercase" };
+            FmtComboBox.Items.AddRange(formats);
+            FmtComboBox.Text = "None";
+        }
+        private int i = 0;
+        private SMSProvider SMSProv;
+        private string Fmt = "None";
+
+        private void timer1_Tick_1(object sender, EventArgs e) {
+            string msg = "Message #" + Convert.ToString(i++);
+            SMSProv.SendSMS(msg, Fmt);
         }
 
-        private void timer1_Tick(object sender, EventArgs e) {
-
+        private void ShowSMS(string str) {
+            MessageListBox.Items.Add(str);
         }
+
+        private void FmtComboBox_SelectedValueChanged(object sender, EventArgs e) {
+            Fmt = FmtComboBox.Text;
+        }
+
+
     }
 }
---
+++ MobilePhoneTests/SMSFormatTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MobilePhoneClassLib;
+using static MobilePhoneClassLib.SMSProvider;
+
+namespace MobilePhoneTests {
+    [TestClass]
+    public class SMSFormatTest {
+        private string Result;
+        [TestMethod]
+        public void SMSFormatterTest() {
+            SMSProvider sMSProv = new SMSProvider();
+            sMSProv.SMSRecieved += new SMSRecievedHandler(FakeShowSMS);
+            string msg = "Test message";
+
+            string format = "Uppercase";
+            string expectedUppercaseResult = "TEST MESSAGE";
+            sMSProv.SendSMS(msg, format);
+            Assert.AreEqual(Result, expectedUppercaseResult);
+
+            format = "Lowercase";
+            string expectedLowercaseResult = "test message";
+            sMSProv.SendSMS(msg, format);
+            Assert.AreEqual(Result, expectedLowercaseResult);
+
+            format = "None";
+            string expectedNoneResult = "Test message";
+            sMSProv.SendSMS(msg, format);
+            Assert.AreEqual(Result, expectedNoneResult);
+        }
+        private void FakeShowSMS(string msg) {
+            Result = msg;
+        }
+    }
+}
